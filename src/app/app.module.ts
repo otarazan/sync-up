43f7881ebf903cb9f71cf7dd7786047d7de6a83c
Baran Ureten <baran.ureten@ericsson.com>
2017-11-16 15:06:57 -0500

    merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import {ApiService} from './api.service'
 import { AppComponent } from './app.component';
 import { Http } from '@angular/http';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/teams/teams.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'teams',
+  templateUrl: './teams.component.html',
+  styleUrls: ['./teams.component.css']
+})
+export class TeamsComponent implements OnInit {
+
+  pageTitle:string = 'TEAMS';
+  teamList:any[] = ["Avengers", "BitsBytes", "specTang", "friday", "luso"];
+
+  userStory:any[] = [1,2,3,4,5,6,7,8];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
